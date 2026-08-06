c85a1b8eed6a3d9948f1322de5f64da7d89c924e
Sandra J. Roberts <sjrober4@dplogin21.dp.nccs.nasa.gov>
2026-08-06 16:44:38 -0400

    Update for interface.py modernizing

--- lib/interface.py
+++ lib/interface.py
@@ -1,40 +1,30 @@
-
-import os
 import sys
 import argparse
+
 import mydatetime as dt
 
-def parse_args(args=None):
+
+def build_parser() -> argparse.ArgumentParser:
 
     parser = argparse.ArgumentParser()
-
-#   # required
-#   required = parser.add_argument_group('required arguments')
-
-#   required.add_argument(
-#       '--rc', metavar='RESOURCE FILE', required=True,
-#       help='Name of resource file'
-#   )
-
-    # optional
     parser.add_argument(
         '--rc', metavar='RESOURCE', default='',
         help='Name of resource file (default: %(default)s)'
     )
     parser.add_argument(
-        '--config', metavar='CONFIG', default=[],action='append',
+        '--config', metavar='CONFIG', default=[], action='append',
         help='Name of configuration file or directory'
     )
     parser.add_argument(
-        '--reset', metavar='RESET', default=[],action='append',
+        '--reset', metavar='RESET', default=[], action='append',
         help='Name of resources to reset'
     )
     parser.add_argument(
-        '--motif', metavar='MOTIF', default=[],action='append',
+        '--motif', metavar='MOTIF', default=[], action='append',
         help='Name of motif file or directory'
     )
     parser.add_argument(
-        '--theme', metavar='THEME', default=[],action='append',
+        '--theme', metavar='THEME', default=[], action='append',
         help='Name of configuration file or directory referencing a theme'
     )
     parser.add_argument(
@@ -86,7 +76,7 @@ def parse_args(args=None):
         help='Ending time in ISO format'
     )
     parser.add_argument(
-        '--t_deltat', metavar='HOURS', type=int, default='3',
+        '--t_deltat', metavar='HOURS', type=int, default=3,
         help='Time increment in hours (default: %(default)s)'
     )
     parser.add_argument(
@@ -101,124 +91,121 @@ def parse_args(args=None):
         '-b', '--basemap', metavar='BASEMAP', default=None,
         help='Basemap name (default: %(default)s)'
     )
-
     parser.add_argument(
         '--no_label', action='store_true', help='Label on/off flag'
     )
-
     parser.add_argument(
         '--no_logo', action='store_true', help='Logo on/off flag'
     )
-
     parser.add_argument(
         '--tight', action='store_true', help='Tight colorbar'
     )
-
     parser.add_argument(
         '--no_title', action='store_true', help='Title on/off flag'
     )
-
     parser.add_argument(
         '--plot_only', action='store_true', help='Plot only flag'
     )
-
     parser.add_argument(
         '--lights_off', action='store_true', help='Lights on/off flag'
     )
-
     parser.add_argument(
         '--label_size', metavar='LABEL_SIZE', default=None,
         help='Size of lat/lon labels (default: %(default)s)'
     )
-
     parser.add_argument(
         '--tick_label_size', metavar='TICK_LABEL_SIZE', default=None,
         help='Size of tick labels (default: %(default)s)'
     )
-
     parser.add_argument(
-        '--track', metavar='PATHNAME', default=[],action='append',
+        '--track', metavar='PATHNAME', default=[], action='append',
         help='pathname or expression'
     )
-
     parser.add_argument(
         '--navigate', metavar='NAVIGATE', default='on',
         help='navigate on/off (default: %(default)s)'
     )
-
     parser.add_argument(
         '--fullframe', action='store_true', help='Turn full frame on'
     )
 
-    if not len(args):
+    return parser
+
+
+def parse_args(args: list[str] | None = None) -> dict:
+
+    if args is None:
+        args = sys.argv[1:]
+
+    parser = build_parser()
+
+    if not args:
         parser.print_help()
         sys.exit(1)
 
     p_args = vars(parser.parse_args(args))
 
     if p_args['time_dt'] is None:
-        now    = dt.datetime.utcnow()
-        hour   = int(now.hour / 12) * 12
-        hhmmss = "%06d"%(hour*10000,)
-        p_args['time_dt'] = now.strftime('%Y%m%dT' + hhmmss)
+        now = dt.datetime.utcnow()
+        hour = (now.hour // 12) * 12
+        p_args['time_dt'] = now.strftime(f'%Y%m%dT{hour * 10000:06d}')
 
     p_args['time_dt'] = make_dt(p_args['time_dt'])
-
-    if p_args['start_dt'] is None:
-        p_args['start_dt'] = make_dt(p_args['time_dt'])
-    else:
-        p_args['start_dt']  = make_dt(p_args['start_dt'])
-
-    if p_args['end_dt'] is None:
-        p_args['end_dt'] = make_dt(p_args['start_dt'])
-    else:
-        p_args['end_dt']  = make_dt(p_args['end_dt'])
+    p_args['start_dt'] = make_dt(p_args['start_dt'] or p_args['time_dt'])
+    p_args['end_dt'] = make_dt(p_args['end_dt'] or p_args['start_dt'])
 
     if p_args['fcst_dt']:
         p_args['fcst_dt'] = make_dt(p_args['fcst_dt'])
-        p_args['fcst_dt'] = dt.datetime.strptime(p_args['fcst_dt'],'%Y%m%dT%H%M%S')
+        p_args['fcst_dt'] = dt.datetime.strptime(p_args['fcst_dt'], '%Y%m%dT%H%M%S')
 
-    p_args['time_dt']  = dt.datetime.strptime(p_args['time_dt'],'%Y%m%dT%H%M%S')
-    p_args['start_dt'] = dt.datetime.strptime(p_args['start_dt'],'%Y%m%dT%H%M%S')
-    p_args['end_dt']   = dt.datetime.strptime(p_args['end_dt'],'%Y%m%dT%H%M%S')
+    p_args['time_dt'] = dt.datetime.strptime(p_args['time_dt'], '%Y%m%dT%H%M%S')
+    p_args['start_dt'] = dt.datetime.strptime(p_args['start_dt'], '%Y%m%dT%H%M%S')
+    p_args['end_dt'] = dt.datetime.strptime(p_args['end_dt'], '%Y%m%dT%H%M%S')
     p_args['t_deltat'] = dt.timedelta(hours=p_args['t_deltat'])
 
     if p_args.get('fullframe', False):
         p_args['parea'] = '0 11 0 8.5'
 
-#   Record user activity
-
-    s_args = [ v.split('-')[-1] for v in args if str(v)[0] == '-' ]
-    s_args = set(s_args)
-
-    user = {}
-    if {'region', 'r'} & s_args: user['region'] = 1
-    if {'field',  'f'} & s_args: user['field'] = 1
-    if {'plot',   'p'} & s_args: user['plot'] = 1
-    if {'level',  'l'} & s_args: user['level'] = 1
-    if {'stream', 's'} & s_args: user['stream'] = 1
-    if {'oname',  'o'} & s_args: user['oname'] = 1
-    if {'collection', 'c'} & s_args: user['collection'] = 1
-    if {'time_dt'} & s_args: user['time_dt'] = 1
-    if {'start_dt'} & s_args: user['start_dt'] = 1
-    if {'end_dt'} & s_args: user['end_dt'] = 1
-    if {'t_deltat'} & s_args: user['t_deltat'] = 1
-
-    if user.get('plot', 0):
-        user['field'] = 1
-        p_args['field'] = p_args['plot']
-
-    p_args['user'] = user
+    p_args['user'] = _record_user_activity(args, p_args)
 
     return p_args
 
-def make_dt(dt_string):
 
-    if len(dt_string) == 15: return dt_string
+def _record_user_activity(args: list[str], p_args: dict) -> dict:
+
+    s_args = {v.lstrip('-') for v in args if v.startswith('-')}
+
+    tracked_flags = {
+        'region': {'region', 'r'},
+        'field': {'field', 'f'},
+        'plot': {'plot', 'p'},
+        'level': {'level', 'l'},
+        'stream': {'stream', 's'},
+        'oname': {'oname', 'o'},
+        'collection': {'collection', 'c'},
+        'time_dt': {'time_dt'},
+        'start_dt': {'start_dt'},
+        'end_dt': {'end_dt'},
+        't_deltat': {'t_deltat'},
+    }
+
+    user = {key: 1 for key, flags in tracked_flags.items() if flags & s_args}
+
+    if user.get('plot'):
+        user['field'] = 1
+        p_args['field'] = p_args['plot']
+
+    return user
+
+
+def make_dt(dt_string: str) -> str:
+
+    if len(dt_string) == 15:
+        return dt_string
 
     if len(dt_string) <= 8:
         dt_string += 'T000000'
     else:
         dt_string += '000000'
 
-    return dt_string[0:14]
+    return dt_string[:14]
